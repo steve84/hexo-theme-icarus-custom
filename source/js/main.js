e678afc69e4d68143d5b33db6090fe336cc0f2a3
Steve Aschwanden <steve.aschwanden@bluewin.ch>
2019-10-26 08:44:11 +0200

    Redesign navbar for mobile

--- source/js/main.js
+++ source/js/main.js
@@ -93,4 +93,10 @@
         $mask.on('click', toggleToc);
         $('.navbar-main .catalogue').on('click', toggleToc);
     }
+
+    $(".navbar-burger").click(function() {
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        $(".navbar-burger").toggleClass("is-active");
+        $(".navbar-menu").toggleClass("is-active");
+    })
 })(jQuery);
